312fea58c42bd909b3f7d48a23f0860981730d37
Fabian Telschow <ftelschow@ucsd.edu>
2021-01-22 12:16:56 +0100

    several small changes

--- SampleFields/R/MeanFunctions.R
+++ SampleFields/R/MeanFunctions.R
@@ -11,3 +11,40 @@
 # Developer notes:
 # - Style guideline NOT included
 #------------------------------------------------------------------------------#
+
+#' Example mean function in 1D
+#'
+#' Function computing the population mean curve. The function is
+#' *insert formula*
+#' and is used in Telschow & Schwartzman(2021).
+#'
+#' @param x Integer amount of realisations of the random field to be generated.
+#' @return Array containing the realisations of the random field.
+#'
+#' @export
+mu.example1.1d <- function( x = seq( 0, 1, length.out = 100 ) ){
+  sin( 8 * pi * x ) * exp( -3 * x )
+}
+
+#' Example mean function in 2D
+#'
+#' Function computing the population mean curve. The function is
+#' *insert formula*
+#' and is used in Telschow & Schwartzman(2021).
+#'
+#' @param x Integer amount of realisations of the random field to be generated.
+#' @param h Vector locations at which the random field is evaluated.
+#' @return Array containing the realisations of the random field.
+#'
+#' @export
+mu.prod.2d <- function( x = seq( 0, 1, length.out = 100 ), y = NULL ){
+
+  # use the x-values as y-values if nothing else specified
+  if( is.null(y) ){
+    y = x
+  }
+
+  # Compute the mean function
+  outer(x,x, FUN = "*" )
+
+}
--- SampleFields/R/VarFunctions.R
+++ SampleFields/R/VarFunctions.R
@@ -6,6 +6,7 @@
 # Required packages:
 #
 # Contained functions:
+# - var.example1.1d
 # - var.example1.2d
 #
 #------------------------------------------------------------------------------#
@@ -13,13 +14,40 @@
 # - Style guideline NOT included
 #------------------------------------------------------------------------------#
 
-#'Creates a sample from an 2D random field, which is the sum of 2-D Gaussian densities..
+#' Example variance function in 1D
+#'
+#' Function computing the variance. The function is
+#' *insert formula*
+#' and is used in Telschow & Schwartzman(2021).
+#'
 #' @param x Integer amount of realisations of the random field to be generated.
 #' @param h Vector locations at which the random field is evaluated.
 #' @return Array containing the realisations of the random field.
 #'
 #' @export
-var.example1.2d <- function( x, h = 1 ){
+var.example1.1d <- function( x = seq( 0, 1, length.out = 100 ), h = 1/6 ){
+  h * ( ( 0.6 - x )^2 + 1 )
+}
+
+#' Example variance function in 2D
+#'
+#' Function computing the variance. The function is
+#' *insert formula*
+#' and is used in Telschow & Schwartzman(2021).
+#'
+#' @param x Integer amount of realisations of the random field to be generated.
+#' @param h Vector locations at which the random field is evaluated.
+#' @return Array containing the realisations of the random field.
+#'
+#' @export
+var.example1.2d <- function( x = seq( 0, 1, length.out = 100 ), y = NULL, h = 1 ){
+
+  # use the x-values as y-values if nothing else specified
+  if( is.null(y) ){
+    y = x
+  }
+
+  # compute the variance function
   h * array( outer( x,
                     x,
                     FUN = function( s, t ) ( s + 1 ) / ( t^2 + 1 ) ) / 3,
